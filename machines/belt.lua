308f65c2d3f88cb31ec704b0cdc90c7aa4691872
theFox6 <the.fox6@gmx.de>
2017-06-16 08:04:51 +0200

    changed belt types
    centering belt is now the default one
    straight belst is the old version

--- machines/belt.lua
+++ machines/belt.lua
@@ -14,9 +14,8 @@ minetest.register_node("factory:belt", {
 		},
 })
 
-minetest.register_node("factory:belt_center", {
-	description = "centering Conveyor Belt",
-	--todo:textures for boards
+minetest.register_node("factory:belt_straight", {
+	description = "straight Conveyor Belt",
 	tiles = {{name="factory_belt_top_animation.png", animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=0.4}}, "factory_belt_bottom.png", "factory_belt_side.png",
 		"factory_belt_side.png", "factory_belt_side.png", "factory_belt_side.png"},
 	groups = {cracky=1},
@@ -25,7 +24,6 @@ minetest.register_node("factory:belt_center", {
 	paramtype2 = "facedir",
 	is_ground_content = true,
 	legacy_facedir_simple = true,
-	--todo: nodebox for boards
 	node_box = {
 			type = "fixed",
 			fixed = {{-0.5,-0.5,-0.5,0.5,0.0625,0.5},}
@@ -33,7 +31,7 @@ minetest.register_node("factory:belt_center", {
 })
 
 minetest.register_abm({
-	nodenames = {"factory:belt", "factory:belt_center"},
+	nodenames = {"factory:belt", "factory:belt_straight"},
 	neighbors = nil,
 	interval = 1,
 	chance = 1,
@@ -134,9 +132,9 @@ minetest.register_entity("factory:moving_item", {
 		local napos = minetest.get_node(pos) 
 		local dir = vector.new(minetest.facedir_to_dir(napos.param2)) -- a copy of the facedir so we don't overwrite the facedir table
 		local speed = 0.8
-		if napos.name == "factory:belt" then
+		if napos.name == "factory:belt_straight" then
 			self.object:setvelocity({x = dir.x / speed, y = 0, z = dir.z / speed})
-		elseif napos.name == "factory:belt_center" then
+		elseif napos.name == "factory:belt" then
 			if dir.x == 0 then
 				dir.x = (math.floor(pos.x + 0.5) - pos.x) * 2
 			elseif dir.z == 0 then

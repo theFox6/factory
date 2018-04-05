bc274bf3fac33998b35dd1d281f891e000f02a4b
theFox6 <the.fox6@gmx.de>
2018-04-05 11:12:06 +0200

    loading time is logged

--- init.lua
+++ init.lua
@@ -1,11 +1,17 @@
-print("loading Factory mod")
+local init = os.clock()
+if minetest.settings:get_bool("log_mods") then
+  minetest.log("action", "[MOD] "..minetest.get_current_modname()..": loading")
+else
+  print("[MOD] "..minetest.get_current_modname()..": loading")
+end
+
 factory={
-crafts={},
-empty={item=ItemStack(nil),time=0},
-no_player={is_player=function() return false end},
---pipes={}, -- coming soon
-worldpath=minetest.get_worldpath(),
-modpath=minetest.get_modpath("factory")
+	crafts={},
+	empty={item=ItemStack(nil),time=0},
+	no_player={is_player=function() return false end},
+	--pipes={}, -- coming soon (perhaps...)
+	worldpath=minetest.get_worldpath(),
+	modpath=minetest.get_modpath("factory")
 }
 -- Boilerplate to support localized strings if intllib mod is installed.
 if minetest.get_modpath( "intllib" ) and intllib then
@@ -21,4 +27,10 @@ dofile(factory.modpath.."/machines/init.lua")
 dofile(factory.modpath.."/items/init.lua")
 dofile(factory.modpath.."/decor/init.lua")
 
-print(factory.S("Factory mod is working"))
+--ready
+local time_to_load= os.clock() - init
+if minetest.settings:get_bool("log_mods") then
+  minetest.log("action", string.format("[MOD] "..minetest.get_current_modname()..factory.S(": loaded in %.4f s"), time_to_load))
+else
+  print(string.format("[MOD] "..minetest.get_current_modname()..factory.S(": loaded in %.4f s"), time_to_load))
+end

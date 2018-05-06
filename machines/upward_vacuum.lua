14fd056112e9bbc2b9ee2065f9db96a2c61b65b0
theFox6 <the.fox6@gmx.de>
2018-05-06 11:50:31 +0200

    only get objects inside the radius

--- machines/upward_vacuum.lua
+++ machines/upward_vacuum.lua
@@ -68,15 +68,12 @@ minetest.register_abm({
 	interval = 1,
 	chance = 1,
 	action = function(pos, node, active_object_count, active_object_count_wider)
-		local all_objects = minetest.get_objects_inside_radius({x = pos.x, y = pos.y-1, z = pos.z}, 1)
+		local all_objects = minetest.get_objects_inside_radius({x = pos.x, y = pos.y-1, z = pos.z}, 0.5)
 		local inv = minetest.get_meta(pos):get_inventory()
 		for _,obj in ipairs(all_objects) do
 			if not obj:is_player() and obj:get_luaentity() and obj:get_luaentity().name == "__builtin:item" then
 				if ItemStack(obj:get_luaentity().itemstring):get_name() == inv:get_list("sort")[1]:get_name() then
-					local dir = vector.subtract(pos,obj:getpos())
-					dir = vector.multiply(dir,0.2)
-					local cpos = vector.add(obj:getpos(),dir)
-					factory.do_moving_item(cpos, obj:get_luaentity().itemstring)
+					factory.do_moving_item(obj:getpos(), obj:get_luaentity().itemstring)
 					obj:get_luaentity().itemstring = ""
 					obj:remove()
 				end

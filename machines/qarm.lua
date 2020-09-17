4675965f567b82d877afc1cb995d907d8295359b
mazes 80 <1608580+mazes-80@users.noreply.github.com>
2020-09-17 18:35:12 +0200

    Deprecated: getters and setters for velocity and pos

--- machines/qarm.lua
+++ machines/qarm.lua
@@ -27,7 +27,7 @@ local function qarm_handle (a, b, target, stack, minv, obj)
 	end
 	if not found then
 		if not insert(minv,"main", stack, obj) then
-			obj:setvelocity({x=0,y=0,z=0})
+			obj:set_velocity({x=0,y=0,z=0})
 			obj:moveto({x = b.x + a.x, y = b.y + 0.5, z = b.z + a.z}, false)
 		end
 	end
